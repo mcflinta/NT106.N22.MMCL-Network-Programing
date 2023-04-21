b1464bacbab9df0c6b0723d25e8c0b843dd3c346
mcflinta <21521901@gm.uit.edu.vn>
2023-04-21 13:30:35 +0700

    add file bai 5

--- Practice/Lab 2/Lab02_NT106/Bai5.Designer.cs
+++ Practice/Lab 2/Lab02_NT106/Bai5.Designer.cs
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnCompressToZip = new System.Windows.Forms.Button();
+            this.btnCompressToGzip = new System.Windows.Forms.Button();
+            this.btnDecompressZip = new System.Windows.Forms.Button();
+            this.btnDecompressGzip = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnCompressToZip
+            // 
+            this.btnCompressToZip.Location = new System.Drawing.Point(18, 13);
+            this.btnCompressToZip.Name = "btnCompressToZip";
+            this.btnCompressToZip.Size = new System.Drawing.Size(103, 56);
+            this.btnCompressToZip.TabIndex = 0;
+            this.btnCompressToZip.Text = "Compress to Zip";
+            this.btnCompressToZip.UseVisualStyleBackColor = true;
+            this.btnCompressToZip.Click += new System.EventHandler(this.btnCompressToZip_Click);
+            // 
+            // btnCompressToGzip
+            // 
+            this.btnCompressToGzip.Location = new System.Drawing.Point(18, 98);
+            this.btnCompressToGzip.Name = "btnCompressToGzip";
+            this.btnCompressToGzip.Size = new System.Drawing.Size(103, 56);
+            this.btnCompressToGzip.TabIndex = 1;
+            this.btnCompressToGzip.Text = "Compress to Gzip";
+            this.btnCompressToGzip.UseVisualStyleBackColor = true;
+            this.btnCompressToGzip.Click += new System.EventHandler(this.btnCompressToGzip_Click);
+            // 
+            // btnDecompressZip
+            // 
+            this.btnDecompressZip.Location = new System.Drawing.Point(159, 13);
+            this.btnDecompressZip.Name = "btnDecompressZip";
+            this.btnDecompressZip.Size = new System.Drawing.Size(103, 56);
+            this.btnDecompressZip.TabIndex = 2;
+            this.btnDecompressZip.Text = "Decompress Zip";
+            this.btnDecompressZip.UseVisualStyleBackColor = true;
+            this.btnDecompressZip.Click += new System.EventHandler(this.btnDecompressZip_Click);
+            // 
+            // btnDecompressGzip
+            // 
+            this.btnDecompressGzip.Location = new System.Drawing.Point(159, 98);
+            this.btnDecompressGzip.Name = "btnDecompressGzip";
+            this.btnDecompressGzip.Size = new System.Drawing.Size(103, 56);
+            this.btnDecompressGzip.TabIndex = 3;
+            this.btnDecompressGzip.Text = "Decompress GZip";
+            this.btnDecompressGzip.UseVisualStyleBackColor = true;
+            this.btnDecompressGzip.Click += new System.EventHandler(this.btnDecompressGzip_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(18, 175);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(244, 23);
+            this.progressBar.TabIndex = 4;
+            // 
+            // Bai5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(272, 211);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnDecompressGzip);
+            this.Controls.Add(this.btnDecompressZip);
+            this.Controls.Add(this.btnCompressToGzip);
+            this.Controls.Add(this.btnCompressToZip);
+            this.Name = "Bai5";
             this.Text = "Bai5";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCompressToZip;
+        private System.Windows.Forms.Button btnCompressToGzip;
+        private System.Windows.Forms.Button btnDecompressZip;
+        private System.Windows.Forms.Button btnDecompressGzip;
+        private System.Windows.Forms.ProgressBar progressBar;
     }
 }
--- Practice/Lab 2/Lab02_NT106/Bai5.cs
+++ Practice/Lab 2/Lab02_NT106/Bai5.cs
@@ -3,18 +3,150 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using ICSharpCode.SharpZipLib.Zip;
+using ICSharpCode.SharpZipLib.GZip;
 namespace Lab02_NT106
-{
+{   
     public partial class Bai5 : Form
     {
         public Bai5()
         {
             InitializeComponent();
+            
+        }
+        
+        /// <summary>
+        /// Hàm nén file thành .zip
+        /// </summary>
+        /// <param name="inputFile"></param>
+        /// <param name="outputFile"></param>
+        public static void CompressToZip(string inputFile, string outputFile)
+        {
+            using (var fsIn = new FileStream(inputFile, FileMode.Open, FileAccess.Read))
+            using (var fsOut = new FileStream(outputFile, FileMode.Create,  FileAccess.Write))
+            {
+                var zipStream = new ZipOutputStream(fsOut);
+                zipStream.PutNextEntry(new ZipEntry(Path.GetFileName(inputFile)));
+                byte[] buffer = new byte[4096];
+                int bytesRead = 0;
+                while((bytesRead = fsIn.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    zipStream.Write(buffer, 0, bytesRead);
+                }
+                zipStream.CloseEntry();
+                zipStream.Close();
+            }
+        }
+        /// <summary>
+        /// Hàm nén file thành .gz
+        /// </summary>
+        /// <param name="inputFile"></param>
+        /// <param name="outputFile"></param>
+        public static void CompressToGZip(string inputFile, string outputFile)
+        {
+            using(var fsIn = new FileStream(inputFile, FileMode.Open, FileAccess.Read))
+            using(var fsOut =  new FileStream(outputFile, FileMode.Create, FileAccess.Write))
+            {
+                var gzipStream = new GZipOutputStream(fsOut);
+                
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while((bytesRead = fsIn.Read(buffer,0, buffer.Length)) > 0)
+                {
+                    gzipStream.Write(buffer, 0, bytesRead);
+                }
+        
+                gzipStream.Close();
+            }
+        }
+        
+        private void btnCompressToZip_Click(object sender, EventArgs e)
+        {
+            string inputFile = "input5.txt";
+            string outputFile = "output5.zip";
+            CompressToZip(inputFile, outputFile);
+            MessageBox.Show("Tập tin đã được nén thành file Zip thành công");
+        }
+
+        private void btnCompressToGzip_Click(object sender, EventArgs e)
+        {
+            string inputFile = "input5.txt";
+            string outputFile = "output5.gz";
+            CompressToGZip(inputFile, outputFile);
+            MessageBox.Show("Tập tin đã được nén file Gzip thành công");
+        }
+        public static void Decompress(string inputFile, string outputFile, ProgressBar progressBar)
+        {
+            using (var fsIn = new FileStream(inputFile, FileMode.Open, FileAccess.Read))
+            using (var fsOut = new FileStream(outputFile, FileMode.Create, FileAccess.Write))
+            {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                long totalBytesRead = 0;
+                long fileSize = fsIn.Length;
+                if (Path.GetExtension(inputFile).Equals(".zip", StringComparison.OrdinalIgnoreCase))
+                {
+                    var zipStream = new ZipInputStream(fsIn);
+                    zipStream.GetNextEntry();
+                    while ((bytesRead = zipStream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        fsOut.Write(buffer, 0, bytesRead);
+                        totalBytesRead += bytesRead;
+                        long progressPercentage = (totalBytesRead * 100L / fileSize);
+                        if (progressPercentage > progressBar.Maximum)
+                        {
+                            progressBar.Value = progressBar.Maximum;
+                        }
+                        else
+                        {
+                            progressBar.Value = (int)progressPercentage;
+                        }
+                    }
+                    zipStream.Close();
+                }
+                else if (Path.GetExtension(inputFile).Equals(".gz", StringComparison.OrdinalIgnoreCase))
+                {
+                    var gzipStream = new GZipInputStream(fsIn);
+                    while ((bytesRead = gzipStream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        fsOut.Write(buffer, 0, bytesRead);
+                        totalBytesRead += bytesRead;
+                        long progressPercentage = (totalBytesRead * 100L / fileSize);
+                        if (progressPercentage > progressBar.Maximum)
+                        {
+                            progressBar.Value = progressBar.Maximum;
+                        }
+                        else
+                        {
+                            progressBar.Value = (int)progressPercentage;
+                        }
+                    }
+                    gzipStream.Close();
+                }
+            }
+
+        }
+
+        private void btnDecompressZip_Click(object sender, EventArgs e)
+        {
+            string inputFile = "output5.zip";
+            string outputFile = "output5.txt";
+            Decompress(inputFile, outputFile, progressBar);
+            MessageBox.Show("Tập tin đã được giải nén thành công");
+        }
+
+        private void btnDecompressGzip_Click(object sender, EventArgs e)
+        {
+            string inputFile = "output5.gz";
+            string outputFile = "output.txt";
+            Decompress(inputFile, outputFile, progressBar);
+            MessageBox.Show("Tập tin đã được giải nén thành công");
         }
     }
 }
